fb2ee29531494f7534f26799ff74f95ea069df27
San Juan <rsanjuan1987@gmail.com>
2020-12-18 13:33:05 -0300

    Update picta-dl.rb

--- picta-dl.rb
+++ picta-dl.rb
@@ -1,18 +1,24 @@
 
 class PictaDl < Formula
+  
+  version 2020.12.14
+  app picta-dl
+  appremote picta-dl
+  
   desc "Download videos from Picta.cu Plataforma de Contenidos Audiovisuales"
   homepage "https://github.com/rsanjuan87/picta-dl/"
-  url "https://github.com/oleksis/picta-dl/releases/download/v2020.12.14/picta-dl"
+  url "https://github.com/oleksis/picta-dl/releases/download/v#{version}/#{appremote}"
   sha256 ""
   license "Unlicense"
   version "1.0"
 
   def install
     libexec.install Dir['*']
-    bin.install_symlink libexec/"picta-dl" => "picta-dl"
-    system "chmod +x #{bin}/picta-dl"
+    bin.install_symlink libexec/appremote => app"
+    system "chmod +x #{bin}/#{app}"
   end
 
   test do
+    assert_match version, shell_output("picta-dl")
   end
 end
