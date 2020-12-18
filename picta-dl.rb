59cdcc4eac3122306c55ac3d61502180713b3f88
San Juan <rsanjuan1987@gmail.com>
2020-12-18 11:38:31 -0300

    Update picta-dl.rb

--- picta-dl.rb
+++ picta-dl.rb
@@ -1,35 +1,19 @@
-# Documentation: https://docs.brew.sh/Formula-Cookbook
-#                https://rubydoc.brew.sh/Formula
-# PLEASE REMOVE ALL GENERATED COMMENTS BEFORE SUBMITTING YOUR PULL REQUEST!
+
 class PictaDl < Formula
   desc "Download videos from Picta.cu Plataforma de Contenidos Audiovisuales"
-  homepage "https://github.com/oleksis/picta-dl/"
-  url "https://github.com/oleksis/picta-dl/archive/v2020.12.14.tar.gz"
-  sha256 "aa85f3a356f8fb0839f1b6ac7d1f0864da425caace90527d784bfb2d03c4c052"
+  homepage "https://github.com/rsanjuan87/picta-dl/"
+  url "https://github.com/rsanjuan87/picta-dl/archive/master.zip"
+  sha256 ""
   license "Unlicense"
 
-  # depends_on "cmake" => :build
+  depends_on "python@3.9"
 
   def install
-    # ENV.deparallelize  # if your formula fails when building in parallel
-    # Remove unrecognized options if warned by configure
-    system "./configure", "--disable-debug",
-                          "--disable-dependency-tracking",
-                          "--disable-silent-rules",
-                          "--prefix=#{prefix}"
-    # system "cmake", ".", *std_cmake_args
+    libexec.install Dir['*']
+    bin.install_exec_script libexec/"picta-dl" => picta-dl
   end
 
   test do
-    # `test do` will create, run in and delete a temporary directory.
-    #
-    # This test will fail and we won't accept that! For Homebrew/homebrew-core
-    # this will need to be a test that verifies the functionality of the
-    # software. Run the test with `brew test picta-dl`. Options passed
-    # to `brew install` such as `--HEAD` also need to be provided to `brew test`.
-    #
-    # The installed folder is not in the path, so use the entire path to any
-    # executables being tested: `system "#{bin}/program", "do", "something"`.
-    system "false"
+    picta-dl version
   end
 end
