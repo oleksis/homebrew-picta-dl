f81c59f835cc727a9ee5190f650a76cd0aa6679d
San Juan <rsanjuan1987@gmail.com>
2020-12-18 11:44:36 -0300

    Update picta-dl.rb

--- picta-dl.rb
+++ picta-dl.rb
@@ -1,12 +1,10 @@
 
-class "picta-dl" < Formula
+class PictaDL < Formula
   desc "Download videos from Picta.cu Plataforma de Contenidos Audiovisuales"
   homepage "https://github.com/rsanjuan87/picta-dl/"
   url "https://github.com/rsanjuan87/picta-dl/archive/master.zip"
   sha256 ""
   license "Unlicense"
-
-  depends_on "python@3.9"
 
   def install
     libexec.install Dir['*']
@@ -14,6 +12,5 @@ class "picta-dl" < Formula
   end
 
   test do
-    picta-dl version
   end
 end
