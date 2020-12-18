117f16d0d4b033bcf4359b2fb73879fbafc384b2
San Juan <rsanjuan1987@gmail.com>
2020-12-18 16:13:43 -0300

    Update picta-dl.rb

--- picta-dl.rb
+++ picta-dl.rb
@@ -12,8 +12,8 @@ class PictaDl < Formula
   def install
     libexec.install Dir['*']
     system "echo \"python #{libexec}/picta_dl/picta-dl \\$@\"> #{libexec}/picta-dl "
-    bin.install_symlink libexec/"picta-dl" => "picta-dl"
-    libexec.install_symlink libexec"/picta_dl/__main__.py" => libexec"/picta_dl/picta-dl"
+    bin.install_symlink "#{libexec}/picta-dl" => "picta-dl"
+    libexec.install_symlink "#{libexec}/picta_dl/__main__.py" => "#{libexec}/picta_dl/picta-dl"
     system "chmod +x #{bin}/picta-dl"
   end
 
